7fbd793a2d9a1834738de80d60a132c4b16138aa
levee98 <leventet@t-email.hu>
2020-06-06 15:02:22 +0200

    7th

---
+++ 7feladat_modulexport.js
@@ -0,0 +1,24 @@
+// module exports: összeadás, kivonás, szorzás, osztás
+
+const osszeadas = (a, b) => {
+  return a + b;
+};
+
+const kivonas = (a, b) => {
+  return a - b;
+};
+
+const szorzas = (a, b) => {
+  return a * b;
+};
+
+const osztas = (a, b) => {
+  return a / b;
+};
+
+module.exports = {
+  osszeadas,
+  kivonas,
+  szorzas,
+  osztas
+};
